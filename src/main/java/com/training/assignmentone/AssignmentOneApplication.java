388f0295286b3e5999ace5cc1d1dfce6f35e71f5
Ondaphe <arthurondaphe@gmail.com>
2023-05-07 19:18:37 +0400

    Initialisation du projet

---
+++ src/main/java/com/training/assignmentone/AssignmentOneApplication.java
@@ -0,0 +1,13 @@
+package com.training.assignmentone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentOneApplication.class, args);
+    }
+
+}
